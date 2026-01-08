f3f3916d23c27a5de0c0530089fe64e3b8d79898
Ayayay1231 <champ25752@gmail.com>
2026-01-09 03:10:53 +0700

    lab13 1 sed laaa

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,43 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N,double B[]){
+    double sum = 0 ;
+        for(int i = 0; i < N ; i++){
+            sum = A[i]+sum ;
+        }
+        B[0] = sum/N ;
+    
+    double SD = 0 ;
+  
+        for(int k = 0; k < N ;k++){
+            SD = pow(A[k]-B[0],2)+SD ;
+        }
+        B[1] = sqrt(SD/(N));
+
+     long double GM = 1 ;
+        for(int j = 0;j<N;j++){
+            GM = GM*A[j];
+        }
+        B[2] = pow(GM,1.0/N);
+    double HM = 0 ;
+        for(int m = 0 ; m<N ; m++){
+            HM = HM+(1/A[m]) ;
+        }
+
+        B[3] = N/HM;
+        double max = A[0];
+        double min = A[0];
+        for(int q = 0 ;q<N ;q++){
+            if(A[q] > max){
+                max = A[q] ;
+            }
+            if(A[q]<min){
+                min = A[q];
+            }
+            
+        }
+        B[4] = max ;
+        B[5] = min ;
+}   
